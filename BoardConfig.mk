141682fe05b798524f46b1dd065b0d09fda683ab
MrBrause <143131566+MrBrause@users.noreply.github.com>
2023-10-25 22:00:55 +0200

    Update BoardConfig.mk
    Rename from gts28ltexx to gts210ltexx

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,10 +1,10 @@
-LOCAL_PATH := device/samsung/gts28ltexx
+LOCAL_PATH := device/samsung/gts210ltexx
 
 # Include path
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Kernel
-TARGET_KERNEL_CONFIG := lineage_gts28ltexx_defconfig
+TARGET_KERNEL_CONFIG := lineage_gts210ltexx_defconfig
 
 # Properties
 TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
@@ -14,7 +14,7 @@ BOARD_MODEM_TYPE := ss333
 BOARD_PROVIDES_LIBRIL := true
 
 # Recovery
-TARGET_OTA_ASSERT_DEVICE := gts28lte,gts28ltexx
+TARGET_OTA_ASSERT_DEVICE := gts210lte,gts210ltexx
 
 # Add RIL-specific SELINUX policy
 BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
@@ -26,5 +26,5 @@ include device/samsung/gts2-common/BoardConfigCommon.mk
 # Add RIL-specific HIDL manifest _after_ the common one.
 #   Note that LOCAL_PATH gets overwritten by the common
 #   board config so we need to explicitly set here again.
-LOCAL_PATH := device/samsung/gts28ltexx
+LOCAL_PATH := device/samsung/gts210ltexx
 DEVICE_MANIFEST_FILE += $(LOCAL_PATH)/manifest.xml
